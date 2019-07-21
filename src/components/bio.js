41a271f4d598f7e8dc65aa01bb5c0cf76f7f4c32
Richard Bagshaw <me@richardbagshaw.co.uk>
2019-07-22 00:22:05 +0100

    tweak bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,8 +54,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> a freelancer who lives and works in
+        Nottingham, UK
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
           You should follow him on Twitter
