08187f6c670aa8d3be35d86e2dba874474c15fb6
Richard Bagshaw <me@richardbagshaw.co.uk>
2019-07-29 17:29:45 +0100

    add 404 decorations

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import sadGifisSad from "../../static/sad.gif"
 
 class NotFoundPage extends React.Component {
   render() {
@@ -12,7 +13,17 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1 className="text-center text-6xl">404 😔</h1>
+        <h1 className="text-center text-6xl">
+          404{" "}
+          <span role="img" aria-label="sad face">
+            😔
+          </span>
+        </h1>
+        <img
+          src={sadGifisSad}
+          alt="Four Oh Four - So Sorry!"
+          className="w-1/3 rounded-lg shadow-lg mx-auto"
+        />
         <p className="text-center">
           You just hit a route that doesn&#39;t exist... the sadness is real
         </p>
